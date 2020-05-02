3cb1231670b5b3d255771e55b1b6eb31931ff342
ShunyaKato <shun.k0829@gmail.com>
2020-05-02 15:18:04 +0900

    create result vue file and nuxt-link to search button

---
+++ pages/result.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="result"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Result extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
