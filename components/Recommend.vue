ab5e93c1ee96eaf8e68b4fb752b935f0b6bc6370
ShunyaKato <shun.k0829@gmail.com>
2020-05-04 00:22:34 +0900

    add recommend section view

---
+++ components/Recommend.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="recommend">
+    <h2 class="recommend__title">オススメ</h2>
+    <div class="recommend__wrap">
+      <div class="recommend__wrap__content" v-for="(itemData, index) in itemData" :key="index">
+        <a class="recommend__wrap__content__link" :href="itemData.url">
+          <img class="recommend__wrap__content__link__image" :src="itemData.image_url" />
+        </a>
+        <p class="recommend__wrap__content__name">{{itemData.name}}</p>
+        <p class="recommend__wrap__content__price">{{itemData.price}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Recommend extends Vue {
+  get itemData() {
+    return this.$store.state.itemData;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.recommend {
+  background-color: #effffb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 0;
+  &__title {
+    font-size: 20px;
+  }
+  &__wrap {
+    display: flex;
+    justify-content: center;
+    &__content {
+      margin: 10px;
+      width: 200px;
+      &__link {
+        &__image {
+          width: 200px;
+          height: 200px;
+        }
+      }
+      &__name {
+        font-size: 16px;
+      }
+      &__price {
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
