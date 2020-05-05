813290df3d1790a95847f013baa8b75e7c1045f7
ShunyaKato <shun.k0829@gmail.com>
2020-05-05 19:24:23 +0900

    add checklist to feature search section

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="feature">
     <h1 class="feature__title">こだわり</h1>
+    <div class="feature__wrap">
+      <div class="feature__wrap__content" v-for="(feature, index) in featureData" :key="index">
+        <input class="feature__wrap__content__checkbox" type="checkbox" :id="feature.featureId" />
+        <label class="feature__wrap__content__label" :for="feature.featureId">{{feature.name}}</label>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
 
 @Component
-export default class Feature extends Vue {}
+export default class Feature extends Vue {
+  get featureData() {
+    return this.$store.state.featureData;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -17,6 +27,18 @@ export default class Feature extends Vue {}
   align-items: center;
   &__title {
     font-size: 20px;
+  }
+  &__wrap {
+    display: flex;
+    align-items: center;
+    &__content {
+      margin: 0 5px;
+      display: flex;
+      align-items: center;
+      &__checkbox {
+        margin-right: 3px;
+      }
+    }
   }
 }
 </style>
