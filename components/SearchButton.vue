8f8162db0bc3eb9adcfff0f585e475f2a26c1d9f
ShunyaKato <shun.k0829@gmail.com>
2020-05-07 19:19:38 +0900

    add set prefecture params to query

--- components/SearchButton.vue
+++ components/SearchButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search-button">
-    <nuxt-link to="/result">
+    <nuxt-link :to="{path: 'result', query: {prefectures: prefectureParams}}">
       <button>検索</button>
     </nuxt-link>
   </div>
@@ -9,7 +9,15 @@
 import { Component, Vue } from "nuxt-property-decorator";
 
 @Component
-export default class SearchBotton extends Vue {}
+export default class SearchBotton extends Vue {
+  get searchParams() {
+    return this.$store.state.searchParams;
+  }
+
+  get prefectureParams() {
+    return this.searchParams.prefectures.toString();
+  }
+}
 </script>
 
 <style lang="scss" scoped>
